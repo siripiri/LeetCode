7a913d00b67a373b9f58370c8e8207e8a615b51e
siripiri <sriprasath.cptech@gmail.com>
2024-08-03 01:59:25 +0530

    BTree - isBalanced

--- LeetCode/src/main/java/com/LeetCode/Trees/BinaryTree/BalancedBTree.java
+++ LeetCode/src/main/java/com/LeetCode/Trees/BinaryTree/BalancedBTree.java
@@ -8,15 +8,46 @@ public class BalancedBTree {
     }
 
     public boolean isBalanced(TreeNode node) {
+        return maxDepth(node) == -1 ? false : true;
+    }
+
+    private int maxDepth(TreeNode node) {
+        if(node == null)
+            return 0;
+
+        // recursively height of left subTree
+        int leftSubTree = maxDepth(node.left);
+        // if leftTree is Unbalanced return -1
+        if(leftSubTree == -1) return -1;
+
+        // recursively height of right subTree
+        int rightSubTree = maxDepth(node.right);
+        // if rightTree is unbalanced return -1
+        if(rightSubTree == -1) return -1;
+
+        // check for balance
+        if(Math.abs(leftSubTree-rightSubTree) > 1) return -1;
+
+        // return height of the tree
+        return Math.max(leftSubTree, rightSubTree)+1;
+    }
+    
+
+    /*
+     *  Check for all nodes if left and right subtree height is atmost 1
+     */
+    public boolean isBalanced1(TreeNode node) {
         if(node == null)
             return true;
         
+        // check for height
         int leftSubTree = this.height.heightRecursive(node.left);
         int rightSubTree = this.height.heightRecursive(node.right);
 
         if(Math.abs(leftSubTree-rightSubTree) > 1)
             return false;
 
+        // Traverse
         boolean left = isBalanced(node.left);
         boolean right = isBalanced(node.right);
 
