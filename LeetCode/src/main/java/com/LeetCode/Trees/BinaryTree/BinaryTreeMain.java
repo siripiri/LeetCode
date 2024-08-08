53ed039a3bbed38880633f7854d5ee9cee1e17ce
siripiri <sriprasath.cptech@gmail.com>
2024-08-09 03:22:47 +0530

    BTree: Level Order Traversal problems

--- LeetCode/src/main/java/com/LeetCode/Trees/BinaryTree/BinaryTreeMain.java
+++ LeetCode/src/main/java/com/LeetCode/Trees/BinaryTree/BinaryTreeMain.java
@@ -1,19 +1,27 @@
 package com.LeetCode.Trees.BinaryTree;
 
-import com.LeetCode.Trees.BinaryTree.traversal.DFS.DFS;
-import com.LeetCode.Trees.BinaryTree.traversal.DFS.RecursiveDFS;
-
+import com.LeetCode.Trees.BinaryTree.traversal.BFS.IterativeBFS;
 public class BinaryTreeMain {
-    private static void print(String val) {
-        System.out.println(val);
-    }
     public static void main(String[] args) {
         Construct con = new Construct();
-        DFS t = new RecursiveDFS();
+        IterativeBFS bfs = new IterativeBFS();
 
-        int[] tree1 = {1,2,3,4,5,6,7,-1,-1,8,-1,-1,-1,9,10};
-        TreeNode root1 = con.constructUsingArray(tree1);
-        con.printTree(root1);
-        con.printBinaryTreeArray(root1);
+        // int[] tree1 = {1,2,3,4,5,6,7,-1,-1,8,-1,-1,-1,9,10};
+        // TreeNode root1 = con.constructUsingArray(tree1);
+        // con.printTree(root1);
+        // con.printBinaryTreeArray(root1);
+
+        int[] tree2 = {1,2,3,4,6,5,7};
+        TreeNode root2 = con.constructUsingArray(tree2);
+        System.out.println(bfs.rightSideView(root2));
+        //con.printBinaryTreeArray(root2);
+        //bfs.traverse(root2);
+        //System.out.println(bfs.verticalTraversal(root2));
+
+
+        // int[] tree3 = {4,2,5,3,-1,7,6,-1,9,-1,-1,8,-1};
+        // TreeNode root3 = con.constructUsingArray(tree3);
+        // System.out.println(t.postOrder(root3));
+
     }
 }
--- LeetCode/src/main/java/com/LeetCode/Trees/BinaryTree/traversal/BFS/IterativeBFS.java
+++ LeetCode/src/main/java/com/LeetCode/Trees/BinaryTree/traversal/BFS/IterativeBFS.java
@@ -3,7 +3,10 @@ package com.LeetCode.Trees.BinaryTree.traversal.BFS;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.TreeMap;
 
 import com.LeetCode.Trees.BinaryTree.TreeNode;
 
@@ -85,6 +88,97 @@ public class IterativeBFS implements BFS {
             level++;
         }
         return list;
+    }
+    class Pair {
+        int rowIndex;
+        int colIndex;
+        TreeNode node;
+        public Pair(int rowIndex, int colIndex, TreeNode node) {
+            this.rowIndex = rowIndex;
+            this.colIndex = colIndex;
+            this.node = node;
+        }
+    }
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if(root == null)
+            return list;
+        Queue<Pair> queue = new LinkedList<>();
+        Map<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        Pair rootPair = new Pair(0, 0, root);
+        queue.add(rootPair);
+
+        while(!queue.isEmpty()) {
+            Pair nodePair = queue.poll();
+
+            if(!map.containsKey(nodePair.colIndex)) {
+                map.put(nodePair.colIndex, new TreeMap<>());
+            }
+
+            if(!map.get(nodePair.colIndex).containsKey(nodePair.rowIndex)){
+                map.get(nodePair.colIndex).put(nodePair.rowIndex , (new PriorityQueue<>()));
+            }
+
+            map.get(nodePair.colIndex).get(nodePair.rowIndex).add(nodePair.node.data);
+
+            if(nodePair.node.left != null) {
+                Pair lefPair = new Pair(nodePair.rowIndex+1, nodePair.colIndex-1, nodePair.node.left);
+                queue.add(lefPair);
+            }
+
+            if(nodePair.node.right != null) {
+                Pair rightPair = new Pair(nodePair.rowIndex+1, nodePair.colIndex+1, nodePair.node.right);
+                queue.add(rightPair);
+            }
+        }
+
+        for(TreeMap<Integer , PriorityQueue<Integer>> column_map : map.values()){
+
+            list.add(new ArrayList<>());
+
+            for(PriorityQueue<Integer> node_values_min_heap : column_map.values()){
+
+                while(!node_values_min_heap.isEmpty()){
+
+                    list.get((list.size() - 1)).add(node_values_min_heap.poll());
+                }
+            }
+        }
+        return list;
+    } 
+    
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        if(root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            List<Integer> subList = new LinkedList<>();
+            for(int i=0; i<size; i++) {
+                TreeNode node = queue.poll();
+
+                subList.add(node.data);
+
+                if(node.left != null)
+                    queue.add(node.left);
+                
+                if(node.right != null)
+                    queue.add(node.right);    
+            }
+            list.add(subList);
+        }
+
+        for(List<Integer> subList: list) {
+            System.out.println(subList);
+            res.add(subList.get(subList.size()-1));
+        }
+
+        return res;
     }
     
 }
